8bd0f340d8c63593ec3b2ac3020554c3566a5729
Paul Cantrell <cantrell@pobox.com>
2013-02-15 14:59:56 -0600

    Faster is fancier!

--- src/edu/macalester/cs124/FancyDemo.java
+++ src/edu/macalester/cs124/FancyDemo.java
@@ -9,7 +9,7 @@ public class FancyDemo extends TurtleProgram {
     public void run() {
         Turtle turtle = new Turtle(getWidth() * 0.5, getHeight() * 0.8, Color.BLUE);
         add(turtle);
-        setTurtleSpeedFactor(1);
+        setTurtleSpeedFactor(0);
         turtle.setPenWidth(1);
         turtle.setStepSize(7);
         
