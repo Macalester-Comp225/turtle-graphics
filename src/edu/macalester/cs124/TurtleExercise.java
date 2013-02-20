158ceea1c97383ba55dcb016e73f2a66d0144302
Paul Cantrell <cantrell@pobox.com>
2013-02-20 12:09:04 -0600

    In-class fiddling

--- src/edu/macalester/cs124/TurtleExercise.java
+++ src/edu/macalester/cs124/TurtleExercise.java
@@ -7,32 +7,32 @@ import edu.macalester.turtle.TurtleProgram;
 
 public class TurtleExercise extends TurtleProgram {
     public void run() {
-        setTurtleSpeedFactor(200);
+        setTurtleSpeedFactor(1000);
         
         Turtle sally = new Turtle(200, 500, Color.MAGENTA);
         add(sally);
         
         Turtle fred = new Turtle(100, 200, Color.GREEN);
-        add(fred);
+        this.add(fred);
         
         sally.forward(2);
-        fred.forward(2.5);
         sally.left(90);
+        fred.forward(2.5);
         sally.forward(3);
-        fred.backward(0.5);
-        fred.right(120);
         sally.left(45);
         sally.setColor(Color.BLUE);
-        fred.forward(2.5);
-        sally.forward(Math.sqrt(8));
-        sally.left(90);
         fred.backward(0.5);
         fred.right(120);
         sally.forward(Math.sqrt(8));
-        sally.setColor(Color.MAGENTA);
-        sally.left(45);
+        sally.left(90);
         fred.forward(2.5);
+        sally.forward(Math.sqrt(8));
+        sally.setColor(Color.MAGENTA);
+        fred.backward(0.5);
+        sally.left(45);
         sally.forward(3);
+        fred.right(120);
+        fred.forward(2.5);
         fred.penUp();
         fred.forward(1);
         fred.penDown();
